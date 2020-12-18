2bf551630dca0b3c09bb3bcb035ec9510d8f476c
ZVasilii <zaitsev.vl@phystech.edu>
2020-12-18 15:35:11 +0000

    BRIIIDDGEEEEE

--- 08_exam/bridge.c
+++ 08_exam/bridge.c
@@ -22,16 +22,20 @@ struct shm_unit
 {
 	unsigned car_waiting;
 	unsigned ships_waiting;
+};
+
+
+struct shm_bridge
+{
 	unsigned bridge_status;
 };
 
 
+enum {BUFF_SIZE = 4096, SEM_QTY = 5, SHM_SIZE = 1024, PERMISS = 0777, MAX_SHIPS = 3, DELAY_T = 1000000};
+enum sem{CAR, SHIP, BRIDGE, SHARED, SHARED_BR};
 
-enum {BUFF_SIZE = 4096, SEM_QTY = 4, SHM_SIZE = 1024, PERMISS = 0777, MAX_SHIPS = 3, DELAY_T = 1000000};
-enum sem{CAR, SHIP, BRIDGE, SHARED};
-
-void car(int num, int sem_id, struct shm_unit* shar_mem);
-void ship(int num, int sem_id, struct shm_unit* shar_mem);
+void car(int num, int sem_id, struct shm_unit* shar_mem, struct shm_bridge* shar_br);
+void ship(int num, int sem_id, struct shm_unit* shar_mem, struct shm_bridge* shar_br);
 
 void Z(int sem_id, int n_sem);
 void P(int sem_id, int n_sem);
@@ -76,18 +80,35 @@ int main(int argc, char** argv)
     exit(EXIT_FAILURE);
   }
 
+
+  //Creating shared structure number 2
+	int shm_id_2 = shmget(IPC_PRIVATE, SHM_SIZE, PERMISS | IPC_CREAT);
+	if (shm_id_2 < 0)
+	{
+		perror("something wrong with shmget");
+		exit(EXIT_FAILURE);
+	}
+  struct shm_bridge* shar_br = (struct shm_bridge*) calloc (1, sizeof(struct shm_bridge));
+  shar_br = (struct shm_bridge*) shmat(shm_id_2, NULL, 0);
+  if (shar_br == NULL || (void*) shar_br == (void*) -1)
+  {
+    perror("Something wrong with shmat (Bridge)\n");
+    exit(EXIT_FAILURE);
+  }
+
   //Initializing
   shar_mem->car_waiting = 0;
   shar_mem->ships_waiting = 0;
   V(sem_id, SHARED);
   V(sem_id, BRIDGE);
+  V(sem_id, SHARED_BR);
   printf("***STARTING***\n");
  
   
 	V(sem_id, CAR);
 
 	printf("Starting condition: BRIDGE FELL!\n");
-	shar_mem -> bridge_status = 0;
+	shar_br -> bridge_status = 0;
 	usleep(DELAY_T);
 
   
@@ -100,9 +121,9 @@ int main(int argc, char** argv)
 		if (!fork_id)
 		{
 			if (i % 2)
-				ship(i / 2, sem_id, shar_mem);
+				ship(i / 2, sem_id, shar_mem, shar_br);
 			else
-				car((i + 1) / 2, sem_id, shar_mem);
+				car((i + 1) / 2, sem_id, shar_mem, shar_br);
 		}
 	}
 
@@ -122,12 +143,18 @@ int main(int argc, char** argv)
 		exit(EXIT_FAILURE);
 	}
 
+	if (shmctl(shm_id_2, IPC_RMID,  (struct shmid_ds*) shar_mem) < 0)
+	{
+		perror("Something wrong with shmctl\n");
+		exit(EXIT_FAILURE);
+	}
+
 	printf("***FINISHING***\n");
 
 	return 0;
 }
 
-void car(int num, int sem_id, struct shm_unit* shar_mem)
+void car(int num, int sem_id, struct shm_unit* shar_mem, struct shm_bridge* shar_br)
 {
 
 	#ifdef DELAY
@@ -149,18 +176,24 @@ void car(int num, int sem_id, struct shm_unit* shar_mem)
 	P(sem_id, SHARED);
 	if (shar_mem->ships_waiting > MAX_SHIPS)
 	{
-		if (shar_mem -> bridge_status != 1)
+		V(sem_id, SHARED);
+		P(sem_id, SHARED_BR);
+		if (shar_br -> bridge_status != 1)
 			  	printf("***Bridge: RISING!***\n");
-					shar_mem -> bridge_status = 1;
+					shar_br -> bridge_status = 1;
 					#ifdef DELAY
 					usleep(DELAY_T);
 					#endif
 					printf("***Bridge: RISED!***\n");
+		V(sem_id, SHARED_BR);
 		V(sem_id, SHIP);
 		V(sem_id, BRIDGE);
 
 	}
-	V(sem_id, SHARED);
+	else
+	{
+		V(sem_id, SHARED);
+	}
 
 	printf("Car #%d!, passing the brige!\n", num);
 
@@ -175,26 +208,29 @@ void car(int num, int sem_id, struct shm_unit* shar_mem)
 
 		if (shar_mem->ships_waiting > MAX_SHIPS || ((shar_mem->car_waiting == 0) && (shar_mem->ships_waiting != 0)))
 		{
-			if (shar_mem -> bridge_status != 1)
+			V(sem_id, SHARED);
+			P(sem_id, SHARED_BR);
+			if (shar_br -> bridge_status != 1)
 			  	printf("***Bridge: RISING!***\n");
-					shar_mem -> bridge_status = 1;
+					shar_br -> bridge_status = 1;
 					#ifdef DELAY
 					usleep(DELAY_T);
 					#endif
 					printf("***Bridge: RISED!***\n");
+			V(sem_id, SHARED_BR);
 			V(sem_id, SHIP);
 		}
 		else
 		{
+			V(sem_id, SHARED);
 			V(sem_id, CAR);
 		}
-	V(sem_id, SHARED);
 	V(sem_id, BRIDGE);
 	//******CRITICAL SECTION*****
 	exit(0);
 }
 
-void ship(int num, int sem_id, struct shm_unit* shar_mem)
+void ship(int num, int sem_id, struct shm_unit* shar_mem, struct shm_bridge* shar_br)
 {
 
 	#ifdef DELAY
@@ -235,20 +271,23 @@ void ship(int num, int sem_id, struct shm_unit* shar_mem)
 
 		if (shar_mem->ships_waiting != 0)
 		{
+			V(sem_id, SHARED);
 			V(sem_id, SHIP);
 		}
 		else
 		{
+			V(sem_id, SHARED);
 			V(sem_id, CAR);
+			P(sem_id, SHARED_BR);
 	  	printf("***Bridge: FALLING!***\n");
-	  	shar_mem -> bridge_status = 0;
+	  	shar_br -> bridge_status = 0;
 	  	#ifdef DELAY
 	  	usleep(DELAY_T);
 	  	#endif
 	  	printf("***Bridge: FELL!***\n");
+	  	V(sem_id, SHARED_BR);
 
 		}
-	V(sem_id, SHARED);
 	V(sem_id, BRIDGE);
 	//******CRITICAL SECTION*****
 	exit(0);
